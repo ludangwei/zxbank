0f005fc4ffa90be19400a5d10e8be324f03fd149
fangshuang <971147928@qq.com>
2018-01-31 09:16:16 +0800

    数据中心新接口对接数据更改

--- src/main/java/com/reptile/util/Resttemplate.java
+++ src/main/java/com/reptile/util/Resttemplate.java
@@ -12,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -39,6 +40,7 @@ public class Resttemplate {
           MediaType type = MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8");
           headers.setContentType(type);
           headers.add("Accept", MediaType.APPLICATION_JSON.toString());
+          logger.warn("推送信息为："+JSONObject.fromObject(map).toString());
           System.out.println(JSONObject.fromObject(map).toString()+"sssvvvv");
           HttpEntity<String> formEntity = new HttpEntity<String>(JSONObject.fromObject(map).toString(), headers);
           String result = restTemplate.postForObject(Url, formEntity,String.class);
@@ -60,5 +62,61 @@ public class Resttemplate {
 	  	return message;
 	  
   }
+  
+  
+  /**
+	 * 数据中心新推口推送方法
+	 * @param map 需要推送的数据
+	 * @param Url 推送的地址
+	 * @return 返回推送状态
+	 */
+  public Map<String,Object> NewSendMessage(Map<String,Object> map,String Url){
+	  logger.warn("入参信息为："+JSONObject.fromObject(map).toString());
+	  Map<String,Object> message=new HashMap<String, Object>();
+	  try {
+		  
+		  	String par="";
+		  	StringHttpMessageConverter m = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+		  	RestTemplate restTemplate = new RestTemplateBuilder().additionalMessageConverters(m).build();
+		  	HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+	        MediaType type = MediaType.parseMediaType("application/x-www-form-urlencoded; charset=UTF-8");
+	        headers.setContentType(type);
+	        headers.add("Accept", MediaType.APPLICATION_JSON.toString());	        
+	        Map<String,String> datamap = new HashMap<String,String>();
+	        datamap.put("data", JSONObject.fromObject(map).toString());
+	        
+	        if(datamap!=null){
+		        Iterator<String> iter = datamap.keySet().iterator(); 
+		          while(iter.hasNext()){ 
+		              String key=iter.next(); 
+		              Object value = datamap.get(key);
+		             par=par+key+"="+value+"&";
+		          }
+		          par=par.substring(0,par.length()-1);
+		    }	        
+	        logger.warn("推送信息为："+par);
+	        HttpEntity<String> formEntity = new HttpEntity<String>(par, headers);
+	        String result = restTemplate.postForObject(Url, formEntity,String.class);
+	        JSONObject jsonObject= JSONObject.fromObject(result);
+	        if(jsonObject.get("errorCode").equals("0000")){
+	      		message.put("errorCode","0000");
+	  			message.put("errorInfo","查询成功");
+	        }else{
+	      		message.put("errorCode",jsonObject.get("errorCode"));//异常处理
+	  			message.put("errorInfo",jsonObject.get("errorInfo"));
+	        }
+         
+		} catch (Exception e) {
+	  		logger.warn("推送数据过程中出现错误 loggerError",e);
+	  		e.printStackTrace();
+			message.put("errorCode","0003");//异常处理
+			message.put("errorInfo","推送失败");
+		}
+	  	return message;
+	  
+	}
+  
+  
 
 }
