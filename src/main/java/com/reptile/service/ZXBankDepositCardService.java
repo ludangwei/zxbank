6e390d3c5ce595217d902b461244a6225aaba6ab
xiejiahao <18191573505@163.com>
2018-01-31 17:15:22 +0800

    状态修改

--- src/main/java/com/reptile/service/ZXBankDepositCardService.java
+++ src/main/java/com/reptile/service/ZXBankDepositCardService.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.hoomsun.keyBoard.SendKeys;
 import com.reptile.util.ConstantInterface;
 import com.reptile.util.PushSocket;
 import com.reptile.util.PushState;
